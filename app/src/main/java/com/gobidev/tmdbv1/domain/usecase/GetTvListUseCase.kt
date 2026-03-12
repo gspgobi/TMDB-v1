6f4e1fb95c8e36001f2070de883a382fddaecac2
Gobinath Subramaniam <gobinath.dev@gmail.com>
2026-03-12 21:25:29 +0530

    Create GetTvListUseCase.kt

---
+++ app/src/main/java/com/gobidev/tmdbv1/domain/usecase/GetTvListUseCase.kt
@@ -0,0 +1,15 @@
+package com.gobidev.tmdbv1.domain.usecase
+
+import androidx.paging.PagingData
+import com.gobidev.tmdbv1.domain.model.TvListType
+import com.gobidev.tmdbv1.domain.model.TvShow
+import com.gobidev.tmdbv1.domain.repository.TvRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetTvListUseCase @Inject constructor(
+    private val repository: TvRepository
+) {
+    operator fun invoke(type: TvListType): Flow<PagingData<TvShow>> =
+        repository.getTvList(type)
+}
