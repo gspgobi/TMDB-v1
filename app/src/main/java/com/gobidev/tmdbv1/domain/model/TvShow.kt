766fa93a30a61f41ca40047b05dd3ce1f8a55622
Gobinath Subramaniam <gobinath.dev@gmail.com>
2026-03-16 23:42:05 +0530

    Add Season & Episode data class

--- app/src/main/java/com/gobidev/tmdbv1/domain/model/TvShow.kt
+++ app/src/main/java/com/gobidev/tmdbv1/domain/model/TvShow.kt
@@ -11,6 +11,16 @@ data class TvShow(
     val voteCount: Int
 )
 
+data class Season(
+    val id: Int,
+    val name: String,
+    val overview: String,
+    val posterUrl: String?,
+    val seasonNumber: Int,
+    val episodeCount: Int,
+    val airDate: String
+)
+
 data class TvShowDetails(
     val id: Int,
     val name: String,
@@ -24,11 +34,24 @@ data class TvShowDetails(
     val numberOfEpisodes: Int,
     val genres: List<Genre>,
     val tagline: String?,
-    val status: String?
+    val status: String?,
+    val seasons: List<Season>
 )
 
 data class TvCredits(
     val id: Int,
     val cast: List<CastMember>,
     val crew: List<CrewMember>
+)
+
+data class Episode(
+    val id: Int,
+    val name: String,
+    val overview: String,
+    val episodeNumber: Int,
+    val seasonNumber: Int,
+    val airDate: String,
+    val stillUrl: String?,
+    val rating: Double,
+    val runtime: Int?
 )
