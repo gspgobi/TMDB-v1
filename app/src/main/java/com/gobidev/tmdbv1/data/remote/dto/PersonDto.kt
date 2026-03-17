00882ee155da8a605253a1a9bbc01e8828870eee
Gobinath Subramaniam <gobinath.dev@gmail.com>
2026-03-17 17:48:02 +0530

    Create PersonDto.kt

---
+++ app/src/main/java/com/gobidev/tmdbv1/data/remote/dto/PersonDto.kt
@@ -0,0 +1,34 @@
+package com.gobidev.tmdbv1.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class PersonDetailsResponse(
+    @SerializedName("id") val id: Int,
+    @SerializedName("name") val name: String,
+    @SerializedName("biography") val biography: String?,
+    @SerializedName("birthday") val birthday: String?,
+    @SerializedName("deathday") val deathday: String?,
+    @SerializedName("place_of_birth") val placeOfBirth: String?,
+    @SerializedName("profile_path") val profilePath: String?,
+    @SerializedName("known_for_department") val knownForDepartment: String?,
+    @SerializedName("popularity") val popularity: Double,
+    @SerializedName("gender") val gender: Int
+)
+
+data class PersonCombinedCreditsResponse(
+    @SerializedName("id") val id: Int,
+    @SerializedName("cast") val cast: List<PersonCastCreditDto>
+)
+
+data class PersonCastCreditDto(
+    @SerializedName("id") val id: Int,
+    @SerializedName("title") val title: String?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("character") val character: String?,
+    @SerializedName("media_type") val mediaType: String,
+    @SerializedName("poster_path") val posterPath: String?,
+    @SerializedName("release_date") val releaseDate: String?,
+    @SerializedName("first_air_date") val firstAirDate: String?,
+    @SerializedName("vote_average") val voteAverage: Double?,
+    @SerializedName("popularity") val popularity: Double?
+)
