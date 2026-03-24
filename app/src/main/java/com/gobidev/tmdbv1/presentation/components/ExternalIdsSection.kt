cd18791c2452143e579b19c36d3aaa6bfef458cc
Gobinath Subramaniam <gobinath.dev@gmail.com>
2026-03-25 00:04:49 +0530

    Revert the icons to monochrome

--- app/src/main/java/com/gobidev/tmdbv1/presentation/components/ExternalIdsSection.kt
+++ app/src/main/java/com/gobidev/tmdbv1/presentation/components/ExternalIdsSection.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.annotation.DrawableRes
@@ -53,7 +52,6 @@ fun ExternalIdsSection(
                         Icon(
                             painter = painterResource(link.iconRes),
                             contentDescription = null,
-                            tint = Color.Unspecified,
                             modifier = Modifier.size(AssistChipDefaults.IconSize)
                         )
                     }
